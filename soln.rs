e2ecc8d1e6e2773ec76451f5feddaa02c25bf81f
Bart Massey <bart.massey@gmail.com>
2022-12-05 01:53:28 -0800

    finished part 1

--- soln.rs
+++ soln.rs
@@ -5,11 +5,64 @@
 //! Advent of Code Day 5.  
 //! Ben and Bart Massey 2022
 
-use aoc::*;
+use aoc::{*, reparse::*};
+use regex::Regex;
+
+fn add_row(rows: &mut [Vec<char>], line: &str) {
+    let line: Vec<char> = line.chars().collect();
+    for (i, col) in rows.iter_mut().enumerate() {
+        let c = line[i * 4 + 1];
+        if c != ' ' {
+            col.push(c);
+        }
+    }
+}
 
 fn main() {
+    let row_re = Regex::new(r"^(\[[A-Z]\]|   )( (\[[A-Z]\]|   ))*$").unwrap();
+
+    // XXX We will take advantage of the input picture
+    // formatting having padding spaces at the end of each
+    // line.
+    let mut lines = input_lines().peekable();
+    let first_row = lines.next().unwrap();
+    assert!(row_re.is_match(&first_row));
+    let nchars = first_row.chars().count();
+    assert_eq!(3, nchars % 4);
+    let ncols = (nchars + 1) / 4;
+    let mut rows: Vec<Vec<char>> = std::iter::repeat(vec![])
+        .take(ncols)
+        .collect();
+
+    add_row(&mut rows, &first_row);
+    while row_re.is_match(lines.peek().unwrap()) {
+        add_row(&mut rows, &lines.next().unwrap());
+    }
+    for row in &mut rows {
+        row.reverse();
+    }
+
+    let _ = lines.next();
+    let _ = lines.next();
+
+    let move_re = Reparse::new(r"move ([0-9]+) from ([0-9]+) to ([0-9]+)");
+    for line in lines {
+        let parsed = move_re.parse(&line).unwrap();
+        let count = parsed.get::<usize>(1);
+        let source = parsed.get::<usize>(2) - 1;
+        let dest = parsed.get::<usize>(3) - 1;
+        for _ in 0..count {
+            let block = rows[source].pop().unwrap();
+            rows[dest].push(block);
+        }
+    }
+
     match get_part() {
-        Part1 => todo!(),
+        Part1 => {
+            let message: String =
+                rows.iter_mut().flat_map(|r| r.pop()).collect();
+            println!("{message}");
+        }
         Part2 => todo!(),
     }
 }
